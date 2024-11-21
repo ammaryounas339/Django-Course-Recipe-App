7d9a30c32153f48d956d8a746e1dada685625a7b
ammaryounas339 <a.younas@algosoftco.com>
2024-11-21 11:21:09 +0500

    tags model and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,10 @@ from decimal import Decimal
 from core import models
 from django.contrib.auth import get_user_model
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     """Test models."""
@@ -44,4 +48,18 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-    
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample receipe description.',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
