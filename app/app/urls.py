058d423aa24cb5ca3415bd8fe5f70256f283a873
ammaryounas339 <a.younas@algosoftco.com>
2024-12-08 12:57:25 +0500

    fixed code that was failing tests

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,7 @@ from django.urls import path,include
 # )
 urlpatterns = [
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
     path('admin/', admin.site.urls),
 
     # path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -10,11 +10,11 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
-
+app_name = 'recipe'
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
-app_name = 'recipe'
+router.register('ingredients', views.IngredientViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
